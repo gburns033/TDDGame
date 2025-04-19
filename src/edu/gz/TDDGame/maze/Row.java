1828b83aa5468cc33776102d71cc895071dbfad8
Zach Goodson <quantamstudios@gmail.com>
2025-04-19 17:36:22 -0500

    Add Row Class

--- src/edu/gz/TDDGame/maze/Row.java
+++ src/edu/gz/TDDGame/maze/Row.java
@@ -1,5 +1,20 @@
 package edu.gz.TDDGame.maze;
 
-public class Row {
+import java.util.ArrayList;
 
+public class Row {
+	private ArrayList<Cell> cells;
+	
+	
+	public Row(ArrayList<Cell> cells) {
+		this.cells = cells;
+	}
+
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+
+	public void setCells(ArrayList<Cell> cellList) {
+		cells = cellList;
+	}
 }
