7278fc427082fcf2b75e9066c59fcff82d5658d0
Gradon Burns <gradyhburns@gmail.com>
2025-04-19 18:35:31 -0500

    Implement Player.java

--- src/edu/gz/TDDGame/player/Player.java
+++ src/edu/gz/TDDGame/player/Player.java
@@ -1,5 +1,22 @@
 package edu.gz.TDDGame.player;
 
-public class Player {
+import edu.gz.TDDGame.maze.Cell;
+import edu.gz.TDDGame.maze.Row;
 
+public class Player {
+	private Row row;
+	private Cell cell;
+
+	public Player(Row row, Cell cell) {
+		this.row = row;
+		this.cell = cell;
+	}
+
+	public Row getCurrentRow() {
+		return this.row;
+	}
+
+	public Cell getCurrentCell() {
+		return this.cell;
+	}
 }
