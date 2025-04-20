17864933f2539c3008a2d2fe59bef08a02bc30f7
Gradon Burns <gradyhburns@gmail.com>
2025-04-19 21:14:58 -0500

    Add comments to the Cell, CellComponents, Grid, and Row classes

--- src/edu/gz/TDDGame/maze/Grid.java
+++ src/edu/gz/TDDGame/maze/Grid.java
@@ -2,27 +2,58 @@ package edu.gz.TDDGame.maze;
 
 import java.util.ArrayList;
 
+/**
+ * Represents the grid of the maze, containing a list of rows.
+ *
+ * The grid is a two-dimensional structure made up of rows, where each row contains multiple cells.
+ */
 public class Grid {
-	private ArrayList<Row> rows;
-		
-	
-	public Grid(ArrayList<Row> rows) {
-		this.rows = rows;
-	}
-	
-	public Integer getIndexOfRow(Row row) {
-		return getRows().indexOf(row);
-	}
-	
-	public Row getRowAt(int index) {
-		return getRows().get(index);
-	}
+    private ArrayList<Row> rows;
 
-	public ArrayList<Row> getRows() {
-		return rows;
-	}
+    /**
+     * Creates a new grid with the specified list of rows.
+     *
+     * @param rows the list of rows that make up the grid
+     */
+    public Grid(ArrayList<Row> rows) {
+        this.rows = rows;
+    }
 
-	public void setRows(ArrayList<Row> rows) {
-		this.rows = rows;
-	}
+    /**
+     * Returns the index of the specified row in the grid.
+     *
+     * @param row the row whose index is to be found
+     * @return the index of the specified row, or -1 if the row is not found
+     */
+    public Integer getIndexOfRow(Row row) {
+        return getRows().indexOf(row);
+    }
+
+    /**
+     * Returns the row at the specified index in the grid.
+     *
+     * @param index the index of the row to retrieve
+     * @return the row at the specified index
+     */
+    public Row getRowAt(int index) {
+        return getRows().get(index);
+    }
+
+    /**
+     * Returns the list of rows in the grid.
+     *
+     * @return the list of rows in the grid
+     */
+    public ArrayList<Row> getRows() {
+        return rows;
+    }
+
+    /**
+     * Sets the list of rows in the grid.
+     *
+     * @param rows the new list of rows to be set in the grid
+     */
+    public void setRows(ArrayList<Row> rows) {
+        this.rows = rows;
+    }
 }
--- src/edu/gz/TDDGame/maze/Row.java
+++ src/edu/gz/TDDGame/maze/Row.java
@@ -2,31 +2,67 @@ package edu.gz.TDDGame.maze;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a row in the maze grid, containing a list of cells.
+ *
+ * Each row holds multiple cells, which can be accessed and manipulated individually.
+ */
 public class Row {
-	private ArrayList<Cell> cells;
-	
-	
-	public Row(ArrayList<Cell> cells) {
-		this.cells = cells;
-	}
-	
+    private ArrayList<Cell> cells;
+
+    /**
+     * Creates a new row with the specified list of cells.
+     *
+     * @param cells the list of cells in the row
+     */
+    public Row(ArrayList<Cell> cells) {
+        this.cells = cells;
+    }
+
+    /**
+     * Returns the cell at the specified index in the row.
+     *
+     * @param index the index of the cell to retrieve
+     * @return the cell at the specified index
+     */
     public Cell getCellAt(int index) {
         return cells.get(index);
     }
 
+    /**
+     * Returns the index of the specified cell in the row.
+     *
+     * @param cell the cell whose index is to be found
+     * @return the index of the specified cell, or -1 if the cell is not found
+     */
     public int getIndexOfCell(Cell cell) {
         return cells.indexOf(cell);
     }
 
+    /**
+     * Returns the number of cells in the row.
+     *
+     * @return the size of the row (i.e., the number of cells)
+     */
     public int getRowSize() {
-    	return cells.size();
+        return cells.size();
     }
-    
-	public ArrayList<Cell> getCells() {
-		return cells;
-	}
 
-	public void setCells(ArrayList<Cell> cellList) {
-		cells = cellList;
-	}
+    /**
+     * Returns the list of cells in the row.
+     *
+     * @return the list of cells in the row
+     */
+    public ArrayList<Cell> getCells() {
+        return cells;
+    }
+
+    /**
+     * Sets the list of cells in the row.
+     *
+     * @param cellList the new list of cells to be set in the row
+     */
+    public void setCells(ArrayList<Cell> cellList) {
+        cells = cellList;
+    }
 }
