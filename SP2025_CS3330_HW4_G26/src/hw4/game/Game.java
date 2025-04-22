eb449b57f73979a7a0238ef76e2804e4ff7e6408
Zach Goodson <quantamstudios@gmail.com>
2025-04-21 21:11:37 -0500

    Handle Index Range Errors and EXIT Case play()

--- SP2025_CS3330_HW4_G26/src/hw4/game/Game.java
+++ SP2025_CS3330_HW4_G26/src/hw4/game/Game.java
@@ -42,41 +42,57 @@ public class Game {
 		Cell targetCell;
 		
 		switch (movement) {
-			case UP:		
+			case UP:	
+				if (rowIndex - 1 < 0) {
+					return false;
+				}
+				
 				targetRow = grid.getRowAt(rowIndex - 1);
 				targetCell = targetRow.getCellAt(colIndex);
 				
 				if (rowIndex != null && rowIndex > 0 && cell.getUp() != CellComponents.WALL && targetCell.getDown() != CellComponents.WALL) {
 					player.setCurrentRow(targetRow);
-					player.setCurrentCell(null);
+					player.setCurrentCell(targetCell);
 					
 					return true;
 				}
 				
 				return false;
 			case DOWN:
+				if (rowIndex + 1 >= grid.getRows().size()) {
+					return false;
+				}
+				
 				targetRow = grid.getRowAt(rowIndex + 1);
 				targetCell = targetRow.getCellAt(colIndex);
 				
 				if (rowIndex != null && rowIndex < row.getRowSize() && cell.getDown() != CellComponents.WALL && targetCell.getUp() != CellComponents.WALL) {
 					player.setCurrentRow(targetRow);
-					player.setCurrentCell(null);
+					player.setCurrentCell(targetCell);
 					
 					return true;
 				}
 				
 				return false;
 			case LEFT:
+				if (colIndex == 0 && cell.getLeft() == CellComponents.EXIT) {
+					return true;
+				}
+				
 				targetCell = row.getCellAt(colIndex - 1);
 				
-				if (colIndex > 0 && cell.getLeft() != CellComponents.WALL && targetCell.getRight() != CellComponents.WALL) {
+				if (colIndex >= 0 && cell.getLeft() != CellComponents.WALL && targetCell.getRight() != CellComponents.WALL) {
 					player.setCurrentCell(targetCell);
 
 					return true;
 				}
 				
 				return false;
-			case RIGHT:		
+			case RIGHT:						
+				if (colIndex + 1 >= row.getRowSize()) {
+					return false;
+				}
+				
 				targetCell = row.getCellAt(colIndex + 1);
 				
 				if (colIndex < row.getCells().size() && cell.getRight() != CellComponents.WALL && targetCell.getLeft() != CellComponents.WALL) {
